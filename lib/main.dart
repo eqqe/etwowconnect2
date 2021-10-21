e3598e87a780b47a6b034c8eeb2e44836399f11c
simon <sfbrey@gmail.com>
2021-10-21 19:57:02 +0200

    Cancel scans when device found, add reconnect button Refacto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -23,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,13 +32,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _flutterReactiveBle = FlutterReactiveBle();
+  final _ble = FlutterReactiveBle();
   final _serviceId = Uuid.parse("0000ffe0-0000-1000-8000-00805f9b34fb");
   final _serviceIdRead = Uuid.parse("0000ffe1-0000-1000-8000-00805f9b34fb");
   final _characteristicId = Uuid.parse("0000ffe1-0000-1000-8000-00805f9b34fb");
+  late StreamSubscription<DiscoveredDevice> listener;
   bool? _locked;
   bool? _lights;
-  int? _trip;
+  int? _odo;
   int? _battery;
   int? _speed;
   String? _id;
@@ -54,36 +48,38 @@ class _MyHomePageState extends State<MyHomePage> {
   bool get _connected =>
       _connectionState?.connectionState == DeviceConnectionState.connected;
 
-  void _resetValues() {
-    _locked = null;
-    _lights = null;
-    _trip = null;
-    _battery = null;
-    _speed = null;
+  bool get _disconnected =>
+      _connectionState?.connectionState == DeviceConnectionState.disconnected;
+
+  void _connect() async {
+    final granted = await Permission.location.request().isGranted &&
+        await Permission.bluetooth.request().isGranted;
+    if (!granted) {
+      return;
+    }
+    if (_id != null) {
+      return listenToDevice(_id!);
+    }
+    listener = _ble.scanForDevices(withServices: []).listen((device) async {
+      if (device.name.contains("E-TWOW")) {
+        setState(() {
+          _id = device.id;
+        });
+        await _ble.requestConnectionPriority(
+            deviceId: device.id, priority: ConnectionPriority.highPerformance);
+        listenToDevice(device.id);
+        listener.cancel();
+      }
+    });
   }
 
   void initState() {
     super.initState();
-    () async {
-      final granted = await Permission.location.request().isGranted;
-      if (!granted) {
-        return;
-      }
-      _flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
-        if (device.name.contains("E-TWOW")) {
-          setState(() {
-            _id = device.id;
-          });
-          listenToDevice(device);
-        }
-      });
-    }();
+    _connect();
   }
 
-  void listenToDevice(DiscoveredDevice device) {
-    _flutterReactiveBle
-        .connectToDevice(id: device.id)
-        .listen((connectionState) {
+  void listenToDevice(String id) {
+    _ble.connectToDevice(id: id).listen((connectionState) {
       setState(() {
         _connectionState = connectionState;
       });
@@ -92,11 +88,17 @@ class _MyHomePageState extends State<MyHomePage> {
             serviceId: _serviceIdRead,
             characteristicId: _characteristicId,
             deviceId: _id!);
-        _flutterReactiveBle
+        _ble
             .subscribeToCharacteristic(characteristic)
             .listen((values) => _updateReadCharacteristics(values));
-      } else {
-        _resetValues();
+      } else if (_disconnected) {
+        setState(() {
+          _locked = null;
+          _lights = null;
+          _odo = null;
+          _battery = null;
+          _speed = null;
+        });
       }
     });
   }
@@ -119,169 +121,144 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     }
     if (values[0] == 5 && values[1] == 1 && values[2] == 0x5f) {
-      _trip = values[3] + values[4] + values[5];
+      _odo = values[3] + values[4] + values[5];
     }
   }
 
-  void _writeCharacteristic(List<int> values) {
+  void _send(List<int> values) {
     if (_id != null && _connected) {
       final characteristic = QualifiedCharacteristic(
           serviceId: _serviceId,
           characteristicId: _characteristicId,
           deviceId: _id!);
       values.add(values.reduce((p, c) => p + c));
-      _flutterReactiveBle.writeCharacteristicWithResponse(characteristic,
-          value: values);
+      _ble.writeCharacteristicWithResponse(characteristic, value: values);
     }
   }
 
   void _lockOn() {
-    _writeCharacteristic([0x55, 0x05, 0x05, 0x01]);
+    _send([0x55, 0x05, 0x05, 0x01]);
   }
 
   void _lockOff() {
-    _writeCharacteristic([0x55, 0x05, 0x05, 0x00]);
+    _send([0x55, 0x05, 0x05, 0x00]);
   }
 
   void _lightOn() {
-    _writeCharacteristic([0x55, 0x06, 0x05, 0x01]);
+    _send([0x55, 0x06, 0x05, 0x01]);
   }
 
   void _lightOff() {
-    _writeCharacteristic([0x55, 0x06, 0x05, 0x00]);
+    _send([0x55, 0x06, 0x05, 0x00]);
   }
 
   void _setSpeed(int mode) {
-    _writeCharacteristic([0x55, 0x02, 0x05, mode]);
-  }
-
-  void _setSpeedL0() {
-    _setSpeed(0);
-  }
-
-  void _setSpeedL1() {
-    _setSpeed(1);
-  }
-
-  void _setSpeedL2() {
-    _setSpeed(2);
-  }
-
-  void _setSpeedL3() {
-    _setSpeed(3);
+    _send([0x55, 0x02, 0x05, mode]);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.lock_open),
-                  color: Colors.green,
-                  tooltip: 'Lock',
-                  onPressed: _locked ?? false ? _lockOff : null,
-                  iconSize: 120,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.lock),
-                  color: Colors.red,
-                  tooltip: 'Lock',
-                  onPressed: _locked ?? true ? null : _lockOn,
-                  iconSize: 120,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.lightbulb),
-                  tooltip: 'Light',
-                  color: Colors.yellow,
-                  onPressed: _lights ?? false ? _lightOff : null,
-                  iconSize: 80,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.lightbulb),
-                  tooltip: 'Light',
-                  onPressed: _lights ?? true ? null : _lightOn,
-                  iconSize: 80,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.speed),
-                  tooltip: '6km/h',
-                  color: Colors.green,
-                  onPressed: _connected ? _setSpeedL1 : null,
-                  iconSize: 70,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.speed),
-                  tooltip: '20km/h',
-                  color: Colors.blue,
-                  onPressed: _connected ? _setSpeedL2 : null,
-                  iconSize: 70,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.speed),
-                  tooltip: '25km/h',
-                  color: Colors.yellow,
-                  onPressed: _connected ? _setSpeedL3 : null,
-                  iconSize: 70,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.speed),
-                  tooltip: '35km/h',
-                  color: Colors.red,
-                  onPressed: _connected ? _setSpeedL0 : null,
-                  iconSize: 70,
-                ),
-              ],
-            ),
-            Text(
-              'MAC: ${_id ?? "searching"}',
-              style: TextStyle(
-                fontSize: 20.0, // insert your font size here
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.lock_open),
+                color: Colors.green,
+                tooltip: 'Lock',
+                onPressed: _locked ?? false ? _lockOff : null,
+                iconSize: 120,
               ),
-            ),
-            Text(
-              '${_speed != null ? "Speed: ${_speed! / 10}" : ""}',
-              style: TextStyle(
-                fontSize: 20.0, // insert your font size here
+              IconButton(
+                icon: const Icon(Icons.lock),
+                color: Colors.red,
+                tooltip: 'Lock',
+                onPressed: _locked ?? true ? null : _lockOn,
+                iconSize: 120,
               ),
-            ),
-            Text(
-              '${_trip != null ? "Trip: $_trip" : ""}',
-              style: TextStyle(
-                fontSize: 20.0, // insert your font size here
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.lightbulb),
+                tooltip: 'Light',
+                color: Colors.yellow,
+                onPressed: _lights ?? false ? _lightOff : null,
+                iconSize: 80,
               ),
-            ),
-            Text(
-              '${_battery != null ? "Battery: $_battery %" : ""}',
-              style: TextStyle(
-                fontSize: 20.0, // insert your font size here
+              IconButton(
+                icon: const Icon(Icons.lightbulb),
+                tooltip: 'Light',
+                onPressed: _lights ?? true ? null : _lightOn,
+                iconSize: 80,
               ),
-            )
-          ],
-        ));
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.speed),
+                tooltip: '6km/h',
+                color: Colors.green,
+                onPressed: _connected ? () => _setSpeed(1) : null,
+                iconSize: 70,
+              ),
+              IconButton(
+                icon: const Icon(Icons.speed),
+                tooltip: '20km/h',
+                color: Colors.blue,
+                onPressed: _connected ? () => _setSpeed(2) : null,
+                iconSize: 70,
+              ),
+              IconButton(
+                icon: const Icon(Icons.speed),
+                tooltip: '25km/h',
+                color: Colors.yellow,
+                onPressed: _connected ? () => _setSpeed(3) : null,
+                iconSize: 70,
+              ),
+              IconButton(
+                icon: const Icon(Icons.speed),
+                tooltip: '35km/h',
+                color: Colors.red,
+                onPressed: _connected ? () => _setSpeed(0) : null,
+                iconSize: 70,
+              ),
+            ],
+          ),
+          Text(
+            'MAC: ${_disconnected ? "lost connection" : (_id ?? "searching")}',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Text(
+            '${_speed != null ? "Speed: ${_speed! / 10}" : ""}',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Text(
+            '${_odo != null ? "Odometer: $_odo" : ""}',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Text(
+            '${_battery != null ? "Battery: $_battery %" : ""}',
+            style: TextStyle(fontSize: 20.0),
+          )
+        ],
+      ),
+      floatingActionButton: _disconnected
+          ? FloatingActionButton.extended(
+              onPressed: _connect,
+              icon: Icon(Icons.bluetooth),
+              backgroundColor: Colors.blue,
+              label: Text("Reconnect"))
+          : null,
+    );
   }
 }
